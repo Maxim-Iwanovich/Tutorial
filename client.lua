c03cdcbfdf49e5912a521eccbbe10504cdab7dc4
Luis-spec <55956668+Luis-spec@users.noreply.github.com>
2021-01-31 18:41:21 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,4 @@
-
+-- Scripting 1
 RegisterCommand('tpw', function(source, args, raw)
     local playerPed = PlayerPedId() -- GetPlayerPed(-1)
     local waypoint = GetFirstBlipInfoId(8)
@@ -6,4 +6,22 @@ RegisterCommand('tpw', function(source, args, raw)
 
     SetEntityCoords(playerPed, waypointCoords.x, waypointCoords.y, waypointCoords.z+35)
 
+end)
+
+
+-- Scripting 2
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(10000)
+        print('do')
+        TriggerEvent('skinchanger:getSkin', function(skinData)
+            if skinData ~= nil then
+                if skinData['beard_2'] > -1 and skinData['beard_2'] < 10 then
+                    skinData['beard_2'] = skinData['beard_2'] + 1
+                    TriggerEvent('skinchanger:loadSkin', skinData)
+                    TriggerServerEvent('esx_skin:save', skinData)
+                end
+            end
+        end)
+    end
 end)
